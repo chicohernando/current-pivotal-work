c7613ef98b2e848528535c022e8c2f72e51faf6c
Eric Sanders <eric.sanders@wellspringworldwide.com>
2018-11-21 11:47:41 -0600

    Made things more configurable and dockerized

--- web/index.php
+++ web/index.php
@@ -1,26 +1,95 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
 
 $app = new Silex\Application();
+$app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/../views',
 ));
 
-$app->get('/key/{key}/project/{project_id}/owner/{owner}', function (Silex\Application $app, $key, $project_id, $owner) use ($app) {
-    if (empty($key)) {
-        $app->abort(404, 'You must supply a pivotal tracker api key');
+$key = $_ENV['PIVOTAL_TRACKER_API_KEY'];
+$project_id = $_ENV['PIVOTAL_TRACKER_PROJECT_ID'];
+$owner = $_ENV['PIVOTAL_TRACKER_OWNER'];
+$team_initials = explode(',', $_ENV['PIVOTAL_TRACKER_TEAM_INITIALS']);
+
+$pivotal_tracker = new \PivotalTrackerV5\Client($key, $project_id);
+
+function populate_owner_initials($stories, $memberships) {
+    foreach ($stories as $story) {
+        $story->owner_initials = array();
+        foreach ($story->owner_ids as $owner_id) {
+            foreach ($memberships as $membership) {
+                if ($membership->person->id == $owner_id) {
+                    $story->owner_initials[] = strtoupper($membership->person->initials);
+                    break;
+                }
+            }
+        }
     }
-    
-    if (empty($project_id)) {
-        $app->abort(404, 'You must supply a project id');
-    }
-    
-    if (empty($owner)) {
-        $app->abort(404, 'You must supply owner initials');
+}
+
+$app->get('/', function(Silex\Application $app) use ($key, $project_id, $pivotal_tracker) {
+    $stories = $pivotal_tracker->getStories('owner:' . $owner . ' -state:unstarted -state:accepted');
+    return $app['twig']->render('index.twig', array(
+        'stories' => $stories,
+    ));
+});
+
+$app->get('/ppp/{starting_label}', function (Request $request, Silex\Application $app, $starting_label) use ($key, $project_id, $pivotal_tracker, $team_initials) {
+    $query_parameters = $request->query->all();
+    $number_of_iterations = isset($query_parameters['number_of_iterations']) ? $query_parameters['number_of_iterations'] : 10;
+    $owners = $team_initials;
+    $epics = array();
+    $points_per_person = array();
+    $sums_per_person = array();
+    foreach ($owners as $owner) {
+        $sums_per_person[$owner] = 0;
     }
 
-    $pivotal_tracker =  new \PivotalTrackerV5\Client($key, $project_id);
+    $date = strtotime($starting_label);
+    for ($i = 0; $i < $number_of_iterations; $i++) {
+        $epics []= date('Y-m-d', $date);
+        $date = strtotime('-2 weeks', $date);
+    }
+
+    foreach ($epics as $epic) {
+        foreach ($owners as $owner) {
+            $points_per_person[$epic][$owner] = 0;
+            $stories = $pivotal_tracker->getStories('owner:' . $owner . ' state:accepted label:' . $epic);
+            foreach ($stories as $story) {
+                $points_per_person[$epic][$owner] += $story->estimate;
+                $sums_per_person[$owner] += $story->estimate;
+            }
+        }
+    }
+
+    foreach ($owners as $owner) {
+        $points_per_person['Average'][$owner] = $sums_per_person[$owner] / $number_of_iterations;
+    }
+
+    $epics []= 'Average';
+
+    return $app['twig']->render('points_per_person.twig', array(
+        'owners' => $owners,
+        'epics' => $epics,
+        'points_per_person' => $points_per_person
+    ));
+});
+
+$app->get('/my', function (Silex\Application $app) use ($key, $project_id, $owner, $pivotal_tracker) {
     $stories = $pivotal_tracker->getStories('owner:' . $owner . ' -state:unstarted -state:accepted');
+    $memberships = $pivotal_tracker->getMemberships();
+    populate_owner_initials($stories, $memberships);
+    return $app['twig']->render('index.twig', array(
+        'stories' => $stories,
+    ));
+});
+
+$app->get('/started', function (Silex\Application $app) use ($key, $project_id, $pivotal_tracker) {
+    $stories = $pivotal_tracker->getStories('state:started');
+    $memberships = $pivotal_tracker->getMemberships();
+    populate_owner_initials($stories, $memberships);
     return $app['twig']->render('index.twig', array(
         'stories' => $stories,
     ));
