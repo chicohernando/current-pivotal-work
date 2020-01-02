6aa9bc39eb19bbc62ad1ae569e3b1d5a82506861
Eric Sanders <ericsanders@wellspringworldwide.com>
2020-01-02 09:20:50 -0600

    Added Nutron team specific stats endpoints

--- web/index.php
+++ web/index.php
@@ -10,8 +10,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $key = $_ENV['PIVOTAL_TRACKER_API_KEY'];
 $project_id = $_ENV['PIVOTAL_TRACKER_PROJECT_ID'];
+$nutron_project_id = $_ENV['PIVOTAL_TRACKER_NUTRON_PROJECT_ID'];
 $owner = $_ENV['PIVOTAL_TRACKER_OWNER'];
 $team_initials = explode(',', $_ENV['PIVOTAL_TRACKER_TEAM_INITIALS']);
+$nutron_team_initials = explode(',', $_ENV['PIVOTAL_TRACKER_NUTRON_TEAM_INITIALS']);
 
 $pivotal_tracker = new \PivotalTrackerV5\Client($key, $project_id);
 
@@ -27,6 +29,17 @@ function populate_owner_initials($stories, $memberships) {
             }
         }
     }
+}
+
+function generate_epics_based_on_starting_label($starting_label, $number_of_iterations) {
+    $epics = array();
+    $date = strtotime($starting_label);
+    for ($i = 0; $i < $number_of_iterations; $i++) {
+        $epics []= date('Y-m-d', $date);
+        $date = strtotime('-2 weeks', $date);
+    }
+
+    return $epics;
 }
 
 $app->get('/', function(Silex\Application $app) use ($key, $project_id, $pivotal_tracker) {
@@ -47,11 +60,7 @@ $app->get('/ppp/{starting_label}', function (Request $request, Silex\Application
         $sums_per_person[$owner] = 0;
     }
 
-    $date = strtotime($starting_label);
-    for ($i = 0; $i < $number_of_iterations; $i++) {
-        $epics []= date('Y-m-d', $date);
-        $date = strtotime('-2 weeks', $date);
-    }
+    $epics = generate_epics_based_on_starting_label($starting_label, $number_of_iterations);
 
     foreach ($epics as $epic) {
         foreach ($owners as $owner) {
@@ -92,6 +101,43 @@ $app->get('/started', function (Silex\Application $app) use ($key, $project_id, 
     populate_owner_initials($stories, $memberships);
     return $app['twig']->render('index.twig', array(
         'stories' => $stories,
+    ));
+});
+
+$app->get('/nutron/ppp/{starting_date}', function (Request $request, Silex\Application $app, $starting_date) use ($key, $nutron_project_id, $nutron_team_initials) {
+    $query_parameters = $request->query->all();
+
+    $number_of_iterations = isset($query_parameters['number_of_iterations']) ? $query_parameters['number_of_iterations'] : 10;
+    $points_per_person = array();
+    $sums_per_person = array();
+    $owners = $nutron_team_initials;
+
+    $epics = generate_epics_based_on_starting_label($starting_date, $number_of_iterations);
+    $pivotal_tracker = new \PivotalTrackerV5\Client($key, $nutron_project_id);
+
+    foreach ($epics as $epic) {
+        $start_timestamp = strtotime($epic);
+        $accepted_filter = 'accepted:' . $epic . '..' . date('Y-m-d', strtotime('+2 saturday', $start_timestamp));
+        foreach ($owners as $owner) {
+            $points_per_person[$epic][$owner] = 0;
+            $stories = $pivotal_tracker->getStories('owner:' . $owner . ' ' . $accepted_filter);
+            foreach ($stories as $story) {
+                $points_per_person[$epic][$owner] += $story->estimate;
+                $sums_per_person[$owner] += $story->estimate;
+            }
+        }
+    }
+
+    foreach ($owners as $owner) {
+        $points_per_person['Average'][$owner] = $sums_per_person[$owner] / $number_of_iterations;
+    }
+
+    $epics []= 'Average';
+
+    return $app['twig']->render('points_per_person.twig', array(
+        'owners' => $owners,
+        'epics' => $epics,
+        'points_per_person' => $points_per_person
     ));
 });
 
