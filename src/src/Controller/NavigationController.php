f636514cb8b2deb8c7d4abf239f9e4426e3f180e
Eric Sanders <eric.sanders@wellspringworldwide.com>
2020-01-13 00:06:14 -0600

    Got projects loading on the home page

--- src/src/Controller/NavigationController.php
+++ src/src/Controller/NavigationController.php
@@ -4,23 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use PivotalTrackerV5\Client;
 
-class HomeController extends AbstractController
+class NavigationController extends AbstractController
 {
-    /**
-     * @Route("/")
-     */
-    public function index()
-    {
+    public function projectList() {
         $pivotal_tracker_client = new Client($_ENV['PIVOTAL_TRACKER_API_KEY'], '');
         $projects = $pivotal_tracker_client->getProjects();
         usort($projects, function($project_1, $project_2) {
             return strcasecmp($project_1->name, $project_2->name);
         });
-        return $this->render('index.html.twig', [
-            'projects' => $projects
-        ]);
+        return $this->render('navigation/_projects.html.twig', array(
+            'projects' => $projects,
+        ));
     }
 }
