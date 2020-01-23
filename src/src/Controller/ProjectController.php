b68a05bbce5a30b2e7f44bf3ef883b9921483755
Eric Sanders <easander@easander.com>
2020-01-22 21:46:23 -0600

    Replaced project and iteration placeholders with real data from the API

--- src/src/Controller/ProjectController.php
+++ src/src/Controller/ProjectController.php
@@ -15,21 +15,17 @@ class ProjectController extends AbstractController
     public function detail(int $project_id)
     {
         $pivotal_tracker_client = new Client($_ENV['PIVOTAL_TRACKER_API_KEY'], $project_id);
-        //TODO replace with getProject call
-        // $projects = $pivotal_tracker_client->getProjects(['fields' => 'name,description,point_scale,current_velocity,current_volatility,created_at']);
-        // $project = array_shift($projects);
-
-        //get iterations and reverse them since they come in oldest to newest order
-        // $iterations = $pivotal_tracker_client->getProjectIterations([
-        //     'limit' => 10,
-        //     'offset' => -9,
-        //     'scope' => 'done_current',
-        //     'fields' => 'number,team_strength,accepted_points,effective_points,velocity,start,finish'
-        // ]);
-        // $iterations = array_reverse($iterations);
-
-        $project = $iterations = [];
+        $project = $pivotal_tracker_client->getProject($project_id, ['fields' => 'id,name,status,version,iteration_length,week_start_day,point_scale,point_scale_is_custom,bugs_and_chores_are_estimatable,automatic_planning,enable_tasks,start_date,time_zone,velocity_averaged_over,shown_iterations_start_time,start_time,number_of_done_iterations_to_show,has_google_domain,description,profile_content,enable_incoming_emails,initial_velocity,project_type,public,current_iteration_number,current_standard_deviation,current_velocity,current_volatility,account_id,created_at,updated_at']);
         
+        //get iterations and reverse them since they come in oldest to newest order
+        $iterations = $pivotal_tracker_client->getProjectIterations([
+            'limit' => 20,
+            'offset' => -19,
+            'scope' => 'done_current',
+            'fields' => 'number,team_strength,accepted_points,effective_points,velocity,start,finish'
+        ]);
+        $iterations = array_reverse($iterations);
+
         return $this->render('project/detail.html.twig', [
             'project' => $project,
             'iterations' => $iterations
