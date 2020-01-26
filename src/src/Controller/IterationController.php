51f3c361642285d2617bff158858972b04c37d7c
Eric Sanders <easander@easander.com>
2020-01-26 17:28:19 -0600

    Removed call to dump

--- src/src/Controller/IterationController.php
+++ src/src/Controller/IterationController.php
@@ -17,7 +17,6 @@ class IterationController extends AbstractController
         $pivotal_tracker_client = new Client($_ENV['PIVOTAL_TRACKER_API_KEY'], $project_id);
         $iteration = $pivotal_tracker_client->getIteration($iteration_id, ['fields' => 'number,team_strength,accepted_points,effective_points,velocity,start,finish,stories(id,name,story_type,estimate,current_state,url,owner_ids),points,accepted,created,analytics']);
 
-        dump($iteration);
         return $this->render('iteration/detail.html.twig', [
             'iteration' => $iteration
         ]);
