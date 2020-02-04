8fe631f000ef9ab50cb3ee3f5b349c8751209e9c
Eric Sanders <easander@easander.com>
2020-02-03 22:42:21 -0600

    Added recent iterations navigation menu when on any page that has a project available

--- src/src/Controller/NavigationController.php
+++ src/src/Controller/NavigationController.php
@@ -16,7 +16,7 @@ class NavigationController extends AbstractController
         });
 
         return $this->render('navigation/_projects.html.twig', [
-            'projects' => $projects,
+            'projects' => $projects
         ]);
     }
 
@@ -26,6 +26,22 @@ class NavigationController extends AbstractController
         
         return $this->render('navigation/_notifications.html.twig', [
             'notifications' => $notifications
+        ]);
+    }
+
+    public function iterationList(int $project_id) {
+        $pivotal_tracker_client = new Client($_ENV['PIVOTAL_TRACKER_API_KEY'], $project_id);
+        $iterations = $pivotal_tracker_client->getProjectIterations([
+            'limit' => 5,
+            'offset' => -4,
+            'scope' => 'done_current',
+            'fields' => 'number,velocity,start,finish'
+        ]);
+        $iterations = array_reverse($iterations);
+
+        return $this->render('navigation/_iterations.html.twig', [
+            'project_id' => $project_id,
+            'iterations' => $iterations
         ]);
     }
 }
